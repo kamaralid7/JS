636441bcd4a159f481c0ac90e191b623641ec061
kamaralid7 <126551638+kamaralid7@users.noreply.github.com>
2023-05-29 15:02:13 +0530

    method1

--- call apply bind/READEME.md
+++ call apply bind/READEME.md
@@ -1,1 +1,7 @@
 # Call Apply and Bind
+
+Using Call method we can do function borrowing
+We can borrow functions from other objests and use data of other objects
+
+Each and every method in JS has special access to call method
+it is usefule to borrow functions from other method
--- call apply bind/index.js
+++ call apply bind/index.js
@@ -1,1 +1,18 @@
-console.log('Hello JS 🚀')
+let name = {
+    firstname: "Kamarali",
+    lastName: "Dukandar",
+    printFullName: function(){
+        console.log(this.firstname + " " + this.lastName);
+    }
+}
+
+let name2 = {
+    firstname: "M.S",
+    lastName: "Dhoni",
+
+}
+
+name.printFullName();
+
+// function borrowing
+name.printFullName.call(name2);
